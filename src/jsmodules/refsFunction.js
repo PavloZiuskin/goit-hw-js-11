bbc24acfb986c5522e698caba0d10493a904ed0d
PavloZiuskin <passzuskin@gmail.com>
2023-01-26 11:17:47 +0200

    add-js-1

---
+++ src/jsmodules/refsFunction.js
@@ -0,0 +1,6 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnMore: document.querySelector('.load-more'),
+  input: document.querySelector('.search-write'),
+};
